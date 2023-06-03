c4c17dd7001de9004a97a4d5d241a441d23f68f2
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-06-03 21:03:50 +0100

    CPU: Move current ARM7 registers to header

--- source/cpu/arm7/cpu_arm7_init.c
+++ source/cpu/arm7/cpu_arm7_init.c
@@ -3,8 +3,6 @@
 
 #include <cpu/arm7/cpu_arm7_init.h>
 
-arm7_registers_whole current_arm7_registers;
-
 int cpu_arm7_init()
 {
     current_arm7_registers.abort_registers.r0 = 0; // Just a lil' value to make sure it works
--- source/cpu/arm7/cpu_arm7_init.h
+++ source/cpu/arm7/cpu_arm7_init.h
@@ -1,6 +1,10 @@
 #ifndef CPU_ARM7_INIT_H_INCLUDED
 #define CPU_ARM7_INIT_H_INCLUDED
 
+#include <types/arm7_types.h>
+
+static arm7_registers_whole current_arm7_registers;
+
 int cpu_arm7_init();
 
 #endif // CPU_ARM7_INIT_H_INCLUDED
