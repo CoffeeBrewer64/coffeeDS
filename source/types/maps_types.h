f57712563e15cc32cabf2508e3bee6bc26ffaa5b
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-06-04 12:34:50 +0100

    Maps: Add maps_types.h

---
+++ source/types/maps_types.h
@@ -0,0 +1,18 @@
+#ifndef MAPS_TYPES_H_INCLUDED
+#define MAPS_TYPES_H_INCLUDED
+
+#include <stdbool.h>
+
+typedef struct
+{
+    const char* map_id;
+    int argument_count;
+    const char* description; // Pull from https://problemkaputt.de/gbatek.htm#dsiomaps
+} nds_io_map;
+
+// TODO: DSi IO map type
+// TODO: DS 3D map type
+// TODO: DS RAM map type
+// TODO: GBA IO map type
+
+#endif // MAPS_TYPES_H_INCLUDED
