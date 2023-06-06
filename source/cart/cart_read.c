2e4edb68af566420d31c1219bca90f9697191865
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-06-06 19:26:01 +0100

    Cart: Replace cartRead_get_ with cart_read_

--- source/cart/cart_read.c
+++ source/cart/cart_read.c
@@ -28,407 +28,407 @@ int cartRead_init()
     return 1;
 }
 
-unsigned char* cartRead_get_gameTitle()
+unsigned char* cart_read_gameTitle()
 {
     return current_header.gameTitle;
 }
 
-unsigned char* cartRead_get_gameCode()
+unsigned char* cart_read_gameCode()
 {
     return current_header.gameCode;
 }
 
-unsigned char* cartRead_get_makerCode()
+unsigned char* cart_read_makerCode()
 {
     return current_header.makerCode;
 }
 
-unsigned char cartRead_get_unitCode()
+unsigned char cart_read_unitCode()
 {
     return current_header.unitCode;
 }
 
-unsigned char* cartRead_get_encryptionSeed()
+unsigned char* cart_read_encryptionSeed()
 {
     return current_header.encryptionSeed;
 }
 
-unsigned char cartRead_get_deviceCapacity()
+unsigned char cart_read_deviceCapacity()
 {
     return current_header.deviceCapacity;
 }
 
-unsigned char* cartRead_get_reserved()
+unsigned char* cart_read_reserved()
 {
     return current_header.reserved;
 }
 
-unsigned char cartRead_get_romVersion()
+unsigned char cart_read_romVersion()
 {
     return current_header.romVersion;
 }
 
-unsigned char cartRead_get_internalFlags()
+unsigned char cart_read_internalFlags()
 {
     return current_header.internalFlags;
 }
 
-unsigned int cartRead_get_arm9RomOffset()
+unsigned int cart_read_arm9RomOffset()
 {
     return current_header.arm9RomOffset;
 }
 
-unsigned int cartRead_get_arm9EntryAddress()
+unsigned int cart_read_arm9EntryAddress()
 {
     return current_header.arm9EntryAddress;
 }
 
-unsigned int cartRead_get_arm9RamAddress()
+unsigned int cart_read_arm9RamAddress()
 {
     return current_header.arm9RamAddress;
 }
 
-unsigned int cartRead_get_arm9Size()
+unsigned int cart_read_arm9Size()
 {
     return current_header.arm9Size;
 }
 
-unsigned int cartRead_get_arm7RomOffset()
+unsigned int cart_read_arm7RomOffset()
 {
     return current_header.arm7RomOffset;
 }
 
-unsigned int cartRead_get_arm7EntryAddress()
+unsigned int cart_read_arm7EntryAddress()
 {
     return current_header.arm7EntryAddress;
 }
 
-unsigned int cartRead_get_arm7RamAddress()
+unsigned int cart_read_arm7RamAddress()
 {
     return current_header.arm7RamAddress;
 }
 
-unsigned int cartRead_get_arm7Size()
+unsigned int cart_read_arm7Size()
 {
     return current_header.arm7Size;
 }
 
-unsigned int cartRead_get_fntOffset()
+unsigned int cart_read_fntOffset()
 {
     return current_header.fntOffset;
 }
 
-unsigned int cartRead_get_fntSize()
+unsigned int cart_read_fntSize()
 {
     return current_header.fntSize;
 }
 
-unsigned int cartRead_get_fatOffset()
+unsigned int cart_read_fatOffset()
 {
     return current_header.fatOffset;
 }
 
-unsigned int cartRead_get_fatSize()
+unsigned int cart_read_fatSize()
 {
     return current_header.fatSize;
 }
 
-unsigned int cartRead_get_arm9OverlayOffset()
+unsigned int cart_read_arm9OverlayOffset()
 {
     return current_header.arm9OverlayOffset;
 }
 
-unsigned int cartRead_get_arm9OverlaySize()
+unsigned int cart_read_arm9OverlaySize()
 {
     return current_header.arm9OverlaySize;
 }
 
-unsigned int cartRead_get_arm7OverlayOffset()
+unsigned int cart_read_arm7OverlayOffset()
 {
     return current_header.arm7OverlayOffset;
 }
 
-unsigned int cartRead_get_arm7OverlaySize()
+unsigned int cart_read_arm7OverlaySize()
 {
     return current_header.arm7OverlaySize;
 }
 
-unsigned short cartRead_get_normalCardControlRegSettings()
+unsigned short cart_read_normalCardControlRegSettings()
 {
     return current_header.normalCardControlRegSettings;
 }
 
-unsigned short cartRead_get_secureCardControlRegSettings()
+unsigned short cart_read_secureCardControlRegSettings()
 {
     return current_header.secureCardControlRegSettings;
 }
 
-unsigned int cartRead_get_iconTitleOffset()
+unsigned int cart_read_iconTitleOffset()
 {
     return current_header.iconTitleOffset;
 }
 
-unsigned short cartRead_get_secureAreaChecksum()
+unsigned short cart_read_secureAreaChecksum()
 {
     return current_header.secureAreaChecksum;
 }
 
-unsigned short cartRead_get_secureAreaDelay()
+unsigned short cart_read_secureAreaDelay()
 {
     return current_header.secureAreaDelay;
 }
 
-unsigned int cartRead_get_arm9AutoLoadListRamAddress()
+unsigned int cart_read_arm9AutoLoadListRamAddress()
 {
     return current_header.arm9AutoLoadListRamAddress;
 }
 
-unsigned int cartRead_get_arm7AutoLoadListRamAddress()
+unsigned int cart_read_arm7AutoLoadListRamAddress()
 {
     return current_header.arm7AutoLoadListRamAddress;
 }
 
-unsigned short cartRead_get_secureAreaDisable()
+unsigned short cart_read_secureAreaDisable()
 {
     return current_header.secureAreaDisable;
 }
 
-unsigned short cartRead_get_ntrRegionRomSize()
+unsigned short cart_read_ntrRegionRomSize()
 {
     return current_header.ntrRegionRomSize;
 }
 
-unsigned short cartRead_get_ntrRegionRuntimeSize()
+unsigned short cart_read_ntrRegionRuntimeSize()
 {
     return current_header.ntrRegionRuntimeSize;
 }
 
-unsigned int cartRead_get_totalRomSize()
+unsigned int cart_read_totalRomSize()
 {
     return current_header.totalRomSize;
 }
 
-unsigned short cartRead_get_romHeaderSize()
+unsigned short cart_read_romHeaderSize()
 {
     return current_header.romHeaderSize;
 }
 
-unsigned short cartRead_get_reserved2()
+unsigned short cart_read_reserved2()
 {
     return current_header.reserved2;
 }
 
-unsigned short cartRead_get_logoChecksum()
+unsigned short cart_read_logoChecksum()
 {
     return current_header.logoChecksum;
 }
 
-unsigned short cartRead_get_headerChecksum()
+unsigned short cart_read_headerChecksum()
 {
     return current_header.headerChecksum;
 }
 
-unsigned int cartRead_get_arm9IconOffset()
+unsigned int cart_read_arm9IconOffset()
 {
     return current_header.arm9IconOffset;
 }
 
-unsigned int cartRead_get_arm9IconSize()
+unsigned int cart_read_arm9IconSize()
 {
     return current_header.arm9IconSize;
 }
 
-unsigned int cartRead_get_arm7IconOffset()
+unsigned int cart_read_arm7IconOffset()
 {
     return current_header.arm7IconOffset;
 }
 
-unsigned int cartRead_get_arm7IconSize()
+unsigned int cart_read_arm7IconSize()
 {
     return current_header.arm9IconSize;
 }
 
-unsigned int cartRead_get_titleLogoOffset()
+unsigned int cart_read_titleLogoOffset()
 {
     return current_header.titleLogoOffset;
 }
 
-unsigned int cartRead_get_titleLogoSize()
+unsigned int cart_read_titleLogoSize()
 {
     return current_header.titleLogoSize;
 }
 
-unsigned int cartRead_get_secureAreaCrc()
+unsigned int cart_read_secureAreaCrc()
 {
     return current_header.secureAreaCrc;
 }
 
-unsigned int cartRead_get_saveType()
+unsigned int cart_read_saveType()
 {
     return current_header.saveType;
 }
 
-unsigned int* cartRead_get_reserved3()
+unsigned int* cart_read_reserved3()
 {
     return current_header.reserved3;
 }
 
-unsigned int cartRead_get_arm9LoadAddress()
+unsigned int cart_read_arm9LoadAddress()
 {
     return current_header.arm9LoadAddress;
 }
 
-unsigned int cartRead_get_arm7LoadAddress()
+unsigned int cart_read_arm7LoadAddress()
 {
     return current_header.arm7LoadAddress;
 }
 
-unsigned char* cartRead_get_reserved4()
+unsigned char* cart_read_reserved4()
 {
     return current_header.reserved4;
 }
 
-unsigned short cartRead_get_region()
+unsigned short cart_read_region()
 {
     return current_header.region;
 }
 
-unsigned char* cartRead_get_reserved5()
+unsigned char* cart_read_reserved5()
 {
     return current_header.reserved5;
 }
 
-unsigned int cartRead_get_arm9OverlayJumpTableOffset()
+unsigned int cart_read_arm9OverlayJumpTableOffset()
 {
     return current_header.arm9OverlayJumpTableOffset;
 }
 
-unsigned int cartRead_get_arm7OverlayJumpTableOffset()
+unsigned int cart_read_arm7OverlayJumpTableOffset()
 {
     return current_header.arm7OverlayJumpTableOffset;
 }
 
-unsigned int* cartRead_get_reserved6()
+unsigned int* cart_read_reserved6()
 {
     return current_header.reserved6;
 }
 
-unsigned int cartRead_get_bannerOffset()
+unsigned int cart_read_bannerOffset()
 {
     return current_header.bannerOffset;
 }
 
-unsigned char* cartRead_get_reserved7()
+unsigned char* cart_read_reserved7()
 {
     return current_header.reserved7;
 }
 
-unsigned int cartRead_get_secureTransferTimeout()
+unsigned int cart_read_secureTransferTimeout()
 {
     return current_header.secureTransferTimeout;
 }
 
-unsigned int cartRead_get_arm9AutoloadListRamAddressEnd()
+unsigned int cart_read_arm9AutoloadListRamAddressEnd()
 {
     return current_header.arm9AutoloadListRamAddressEnd;
 }
 
-unsigned int cartRead_get_arm7AutoloadListRamAddressEnd()
+unsigned int cart_read_arm7AutoloadListRamAddressEnd()
 {
     return current_header.arm7AutoloadListRamAddressEnd;
 }
 
-unsigned char* cartRead_get_reserved8()
+unsigned char* cart_read_reserved8()
 {
     return current_header.reserved8;
 }
 
-unsigned char cartRead_get_ntrRegionRomVersion()
+unsigned char cart_read_ntrRegionRomVersion()
 {
     return current_header.ntrRegionRomVersion;
 }
 
-unsigned char* cartRead_get_reserved9()
+unsigned char* cart_read_reserved9()
 {
     return current_header.reserved9;
 }
 
-unsigned int cartRead_get_defaultTiming()
+unsigned int cart_read_defaultTiming()
 {
     return current_header.defaultTiming;
 }
 
-unsigned int cartRead_get_arm9AutoloadListRamAddressMain()
+unsigned int cart_read_arm9AutoloadListRamAddressMain()
 {
     return current_header.arm9AutoloadListRamAddressMain;
 }
 
-unsigned int cartRead_get_arm7AutoloadListRamAddressMain()
+unsigned int cart_read_arm7AutoloadListRamAddressMain()
 {
     return current_header.arm7AutoloadListRamAddressMain;
 }
 
-unsigned int* cartRead_get_reserved10()
+unsigned int* cart_read_reserved10()
 {
     return current_header.reserved10;
 }
 
-unsigned int cartRead_get_filenameTableOffset()
+unsigned int cart_read_filenameTableOffset()
 {
     return current_header.filenameTableOffset;
 }
 
-unsigned int cartRead_get_filenameTableSize()
+unsigned int cart_read_filenameTableSize()
 {
     return current_header.filenameTableSize;
 }
 
-unsigned int cartRead_get_fileAllocationTableOffset()
+unsigned int cart_read_fileAllocationTableOffset()
 {
     return current_header.fileAllocationTableOffset;
 }
 
-unsigned int cartRead_get_fileAllocationTableSize()
+unsigned int cart_read_fileAllocationTableSize()
 {
     return current_header.fileAllocationTableSize;
 }
 
-unsigned int cartRead_get_filenameTable2Offset()
+unsigned int cart_read_filenameTable2Offset()
 {
     return current_header.filenameTable2Offset;
 }
 
-unsigned int cartRead_get_filenameTable2Size()
+unsigned int cart_read_filenameTable2Size()
 {
     return current_header.filenameTable2Size;
 }
 
-unsigned int cartRead_get_fileAllocationTable2Offset()
+unsigned int cart_read_fileAllocationTable2Offset()
 {
     return current_header.fileAllocationTable2Offset;
 }
 
-unsigned int cartRead_get_fileAllocationTable2Size()
+unsigned int cart_read_fileAllocationTable2Size()
 {
     return current_header.fileAllocationTable2Size;
 }
 
-unsigned int cartRead_get_bannerAnimationOffset()
+unsigned int cart_read_bannerAnimationOffset()
 {
     return current_header.bannerAnimationOffset;
 }
 
-unsigned int* cartRead_get_reserved11()
+unsigned int* cart_read_reserved11()
 {
     return current_header.reserved11;
 }
 
-unsigned char cartRead_get_ndsRegion()
+unsigned char cart_read_ndsRegion()
 {
     return current_header.ndsRegion;
 }
 
-unsigned char* cartRead_get_reserved12()
+unsigned char* cart_read_reserved12()
 {
     return current_header.reserved12;
 }
