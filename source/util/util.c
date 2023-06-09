d5cd11df77578537d4f02a9fa7372649e11c0a08
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-06-09 19:29:28 +0100

    Utils: Add util_file_writeToFile

--- source/util/util.c
+++ source/util/util.c
@@ -13,6 +13,7 @@ Original author: CoffeeBrewer64
 
 #include <util/util_strings.h>
 #include <util/util_log.h>
+#include <util/util_file.h>
 
 bool _isFirstLogFile = true;
 
@@ -65,5 +66,40 @@ void LOG(const char* format, ...)
 
     fclose(file);
     _isFirstLogFile = false;
+    return;
+}
+
+void util_file_writeToFile(const char* fileName, const char* format, ...)
+{
+    FILE* file = fopen(fileName, "a");
+    if (file == NULL)
+    {
+        printf("Error opening log file.\n");
+        return;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    // Check if format ends with a newline character
+    size_t format_len = strlen(format);
+    if (format_len > 0 && format[format_len - 1] != '\n')
+    {
+        // Format does not end with newline, so add it
+        vfprintf(stdout, format, args);
+        vfprintf(file, format, args);
+        printf("\n");
+        fprintf(file, "\n");
+    }
+    else
+    {
+        // Format already ends with newline
+        vprintf(format, args);
+        vfprintf(file, format, args);
+    }
+
+    va_end(args);
+
+    fclose(file);
     return;
 }
---
+++ source/util/util_file.h
@@ -0,0 +1,14 @@
+/*
+Name: util_file.h
+Description: Header file for file management under the utils.
+License: MIT License Copyright (c) CoffeeBrewer64 2023
+Last author: CoffeeBrewer64
+Original author: CoffeeBrewer64
+*/
+
+#ifndef UTIL_FILE_H_INCLUDED
+#define UTIL_FILE_H_INCLUDED
+
+void util_file_writeToFile(const char* fileName, const char* format, ...);
+
+#endif // UTIL_FILE_H_INCLUDED
