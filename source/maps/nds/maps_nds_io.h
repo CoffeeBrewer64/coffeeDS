99cd85e579b4b42adbe3a928597390d98c8b8eee
CoffeeBrewer64 <mrdirtbloc@gmail.com>
2023-06-11 11:15:58 +0100

    Maps: Add ARM9 IPC/ROM maps and remove useless comments

--- source/maps/nds/maps_nds_io.h
+++ source/maps/nds/maps_nds_io.h
@@ -19,14 +19,27 @@ nds_io_map NDS_IO_MAP_4000006h = { "4000006h", 2, "2D Engine A+B - VCOUNT - Vert
 nds_io_map NDS_IO_MAP_4000008h = { "4000008h", 50, "2D Engine A (same registers as GBA, some changed bits)", 2};
 nds_io_map NDS_IO_MAP_4000060h = { "4000060h", 2, "DISP3DCNT - 3D Display Control Register", 2};
 nds_io_map NDS_IO_MAP_4000064h = { "4000064h", 4, "DISPCAPCNT - Display Capture Control Register", 2};
-nds_io_map NDS_IO_MAP_4000068h = { "4000068h", 4, "DISP_MMEM_FIFO - Main Memory Display FIFO", 2}; // Assuming can read
-nds_io_map NDS_IO_MAP_400006Ch = { "400006Ch", 2, "2D Engine A - MASTER_BRIGHT - Master Brightness Up/Down", 2}; // Assuming can read and/or write
+nds_io_map NDS_IO_MAP_4000068h = { "4000068h", 4, "DISP_MMEM_FIFO - Main Memory Display FIFO", 2};
+nds_io_map NDS_IO_MAP_400006Ch = { "400006Ch", 2, "2D Engine A - MASTER_BRIGHT - Master Brightness Up/Down", 2};
 
 /// ARM9 DMA, Timers, and Keypad ///
-nds_io_map NDS_IO_MAP_40000B0h = { "40000B0h", 30, "DMA Channel 0..3", 2}; // Assuming read and write
-nds_io_map NDS_IO_MAP_40000E0h = { "40000E0h", 10, "DMA FILL Registers for Channel 0..3", 2}; // Assuming read and write
-nds_io_map NDS_IO_MAP_4000100h = { "4000100h", 10, "Timers 0..3", 2}; //Assuming read and write
-nds_io_map NDS_IO_MAP_4000130h = { "4000130h", 2, "KEYINPUT", 2}; // Assuming read and write
-nds_io_map NDS_IO_MAP_4000132h = { "4000132h", 2, "KEYCNT", 2}; // Assuming read and write
+nds_io_map NDS_IO_MAP_40000B0h = { "40000B0h", 30, "DMA Channel 0..3", 2};
+nds_io_map NDS_IO_MAP_40000E0h = { "40000E0h", 10, "DMA FILL Registers for Channel 0..3", 2};
+nds_io_map NDS_IO_MAP_4000100h = { "4000100h", 10, "Timers 0..3", 2};
+nds_io_map NDS_IO_MAP_4000130h = { "4000130h", 2, "KEYINPUT", 2};
+nds_io_map NDS_IO_MAP_4000132h = { "4000132h", 2, "KEYCNT", 2};
+
+/// ARM9 IPC/ROM
+nds_io_map NDS_IO_MAP_4000180h = { "4000180h", 2, "IPCSYNC - IPC Synchronize Register", 2};
+nds_io_map NDS_IO_MAP_4000184h = { "4000184h", 2, "IPCFIFOCNT - IPC Fifo Control Register", 2};
+nds_io_map NDS_IO_MAP_4000188h = { "4000188h", 4, "IPCFIFOSEND - IPC Send Fifo", 2};
+nds_io_map NDS_IO_MAP_40001A0h = { "40001A0h", 2, "AUXSPICNT - Gamecard ROM and SPI Control", 2};
+nds_io_map NDS_IO_MAP_40001A2h = { "40001A2h", 2, "AUXSPIDATA - Gamecard SPI Bus Data/Strobe", 2};
+nds_io_map NDS_IO_MAP_40001A4h = { "40001A4h", 4, "Gamecard bus timing/control", 2};
+nds_io_map NDS_IO_MAP_40001A8h = { "40001A8h", 8, "Gamecard bus 8-byte command out", 2};
+nds_io_map NDS_IO_MAP_40001B0h = { "40001B0h", 4, "Gamecard Encryption Seed 0 Lower 32bit", 2};
+nds_io_map NDS_IO_MAP_40001B4h = { "40001B4h", 4, "Gamecard Encryption Seed 1 Lower 32bit", 2};
+nds_io_map NDS_IO_MAP_40001B8h = { "40001B8h", 2, "Gamecard Encryption Seed 0 Upper 7bit", 2};
+nds_io_map NDS_IO_MAP_40001BAh = { "40001BAh", 2, "Gamecard Encryption Seed 1 Upper 7bit", 2};
 
 #endif // MAPS_NDS_IO_H_INCLUDED
